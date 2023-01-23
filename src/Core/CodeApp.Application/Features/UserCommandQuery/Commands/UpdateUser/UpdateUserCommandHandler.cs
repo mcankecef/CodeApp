f3b52e9e989c393fb1237d0abfade1f8a0d2cde5
can33 <75936005+can33@users.noreply.github.com>
2023-01-23 22:01:38 +0300

    added update user action

---
+++ src/Core/CodeApp.Application/Features/UserCommandQuery/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CodeApp.Application.Abstractions;
+using CodeApp.Application.Dtos;
+using CodeApp.Application.Dtos.User;
+using CodeApp.Application.Wrapper;
+using MediatR;
+
+namespace CodeApp.Application.Features.UserCommandQuery.Commands.UpdateUser
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommandRequest, BaseResponse<NoContentDto>>
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UpdateUserCommandHandler(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseResponse<NoContentDto>> Handle(UpdateUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            var updateUserDto = _mapper.Map<UpdateUserDto>(request);    
+
+            await _userService.UpdateUser(updateUserDto);
+
+            return new BaseResponse<NoContentDto>();
+        }
+    }
+}
---
+++ src/Core/CodeApp.Application/Features/UserCommandQuery/Commands/UpdateUser/UpdateUserCommandRequest.cs
@@ -0,0 +1,19 @@
+﻿using CodeApp.Application.Dtos;
+using CodeApp.Application.Wrapper;
+using MediatR;
+
+namespace CodeApp.Application.Features.UserCommandQuery.Commands.UpdateUser
+{
+    public class UpdateUserCommandRequest : IRequest<BaseResponse<NoContentDto>>
+    {
+        public UpdateUserCommandRequest(string userId, string fullName, string? email)
+        {
+            UserId = userId;
+            FullName = fullName;
+            Email = email;
+        }
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string? Email { get; set; }
+    }
+}
