f1ea41179c2e7bef1671d2cf401e262e79be54e1
can33 <75936005+can33@users.noreply.github.com>
2023-01-23 22:01:00 +0300

    added delete user action

---
+++ src/Core/CodeApp.Application/Features/UserCommandQuery/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using CodeApp.Application.Abstractions;
+using CodeApp.Application.Dtos;
+using CodeApp.Application.Wrapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeApp.Application.Features.UserCommandQuery.Commands.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommandRequest, BaseResponse<NoContentDto>>
+    {
+        private readonly IUserService _userService;
+
+        public DeleteUserCommandHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<BaseResponse<NoContentDto>> Handle(DeleteUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            if(request.UserId== null)
+                throw new ArgumentNullException($"{request.UserId} is must not null");
+
+            await _userService.DeleteUser(request.UserId);
+
+            return new BaseResponse<NoContentDto>();
+
+
+        }
+    }
+}
---
+++ src/Core/CodeApp.Application/Features/UserCommandQuery/Commands/DeleteUser/DeleteUserCommandRequest.cs
@@ -0,0 +1,21 @@
+﻿using CodeApp.Application.Dtos;
+using CodeApp.Application.Wrapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeApp.Application.Features.UserCommandQuery.Commands.DeleteUser
+{
+    public class DeleteUserCommandRequest : IRequest<BaseResponse<NoContentDto>>
+    {
+        public string UserId { get; set; }
+
+        public DeleteUserCommandRequest(string userId)
+        {
+            UserId = userId;
+        }
+    }
+}
