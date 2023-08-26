17167ed40bba088f035a940078afc7056777bf09
can33 <cankecef@hotmail.com>
2023-08-27 00:00:13 +0300

    Status type added to Avatar entity.

--- src/Core/CodeApp.Domain/Entities/Avatar.cs
+++ src/Core/CodeApp.Domain/Entities/Avatar.cs
@@ -1,4 +1,5 @@
 ﻿using CodeApp.Domain.Entities.Identity;
+using CodeApp.Domain.Enums;
 
 namespace CodeApp.Domain.Entities
 {
@@ -7,6 +8,7 @@ namespace CodeApp.Domain.Entities
         public Avatar() => AppUsers = new List<AppUser>();
         public string ImageUrl { get; set; }
         public string? Description { get; set; }
+        public StatusType Status { get; set; }
         public ICollection<AppUser> AppUsers { get; set; }
     }
 }
