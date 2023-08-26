8143971e56364ce44cb65017ae3a1ee32c2dfb0e
can33 <cankecef@hotmail.com>
2023-08-27 00:09:41 +0300

    Exception middleware added for global exception handler.

---
+++ src/WebAPI/CodeApp.WebAPI/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+namespace CodeApp.WebAPI.Extensions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = 500;
+
+            await httpContext.Response.WriteAsync(JsonSerializer.Serialize(new
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = exception.Message,
+                Title = "An unexpected error has occurred."
+            }));
+        }
+    }
+}
