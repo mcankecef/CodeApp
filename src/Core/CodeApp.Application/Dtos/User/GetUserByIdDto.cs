c2e864a45cd67569ca07603c4b9731d2dc9e3952
can33 <cankecef@hotmail.com>
2023-09-01 21:15:05 +0300

    AvatarId property added to GetUserById and UpdaterUser dtos.

--- src/Core/CodeApp.Application/Dtos/User/GetUserByIdDto.cs
+++ src/Core/CodeApp.Application/Dtos/User/GetUserByIdDto.cs
@@ -1,9 +1,9 @@
-﻿namespace CodeApp.Application.Dtos.User
+﻿namespace CodeApp.Application.Dtos.User;
+public class GetUserByIdDto
 {
-    public class GetUserByIdDto
-    {
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public int Score { get; set; }
-    }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public int Score { get; set; }
+    public string ImageUrl { get; set; }
+    public Guid AvatarId { get; set; }
 }
--- src/Core/CodeApp.Application/Dtos/User/UpdateUserDto.cs
+++ src/Core/CodeApp.Application/Dtos/User/UpdateUserDto.cs
@@ -3,7 +3,8 @@
     public class UpdateUserDto
     {
         public string UserId { get; set; }
-        public string FullName { get; set; }
+        public string? FullName { get; set; }
         public string? Email { get; set; }
+        public Guid? AvatarId { get; set; }
     }
 }
