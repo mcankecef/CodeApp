09fd02c6040c4099c9f7171923644e67ca105efd
cankecef <cankecef@hotmail.com>
2025-08-06 00:40:09 +0300

    Refactor project structure and update dependencies

--- src/Core/CodeApp.Domain/obj/Debug/net6.0/CodeApp.Domain.AssemblyInfo.cs
+++ src/Core/CodeApp.Domain/obj/Debug/net6.0/CodeApp.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5391203cf4b673b6b13a57a71093f9c57478e0e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
